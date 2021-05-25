be901f033aac3596f509ad7aaa6486efe3e0ca4b
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-05-24 23:06:52 -0300

    Pruebas en ListadoProyectos y actualizacion de jest

---
+++ src/__tests__/projects/ProjectList.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { act, configure, fireEvent, render, screen, waitFor } from "../../utils/test-utils"
+import Project from "../../components/proyectos/Project"
+import '@testing-library/jest-dom'
+import taskService from '../../services/taskService'
+import projectService from '../../services/projectService'
+import ListadoProyectos from '../../components/proyectos/ListadoProyectos'
+
+configure({ asyncUtilTimeout: 5000 })
+beforeAll(() => {
+  jest.mock('../../services/taskService')
+  jest.mock('../../services/projectService')
+})
+
+afterAll(() => {
+  jest.clearAllMocks()
+})
+
+describe('Testing <ListadoProyectos />', () => {
+  const project = {
+    _id: 1,
+    nombre: 'testing a project'
+  }
+  taskService.getTasks = jest.fn().mockResolvedValue({ tasks: [], alert: null })
+  projectService.getProjects = jest.fn().mockResolvedValue({ projects: [project], alert: null })
+
+  test('Debe imprimir el componente', async () => {
+
+    render(<ListadoProyectos />)
+
+    await waitFor(() => {
+      expect(screen.queryByRole('button', { name: project.nombre })).toBeInTheDocument()
+    })
+  })
+
+  test('Debe seleccionarse el proyecto y marcase como activo', async () => {
+
+    render(<ListadoProyectos />)
+    
+    await waitFor(() => {
+      expect(screen.queryByRole('button', { name: project.nombre })).toBeInTheDocument()
+    })
+    
+    act(() => {
+      fireEvent.click(screen.queryByRole('button', { name: project.nombre }))
+    })
+    
+    await waitFor(() => 
+      expect(screen.queryByRole('listitem', { name: project.nombre }).classList.contains('active-project')).toBe(true)
+    )
+  })
+})
